77a7e9086d6a8dfd0a5a8be1231291777518f3f1
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-04-03 15:27:42 +0800

    Add NetworkModule

---
+++ app/src/main/java/com/example/todo_app/di/NetworkModule.kt
@@ -0,0 +1,29 @@
+package com.example.todo_app.di
+
+import com.example.todo_app.api.UserAPI
+import com.example.todo_app.utils.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class NetworkModule {
+
+    @Singleton
+    @Provides
+    fun providesRetrofit(): Retrofit.Builder {
+        return Retrofit.Builder().baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+    }
+
+    @Singleton
+    @Provides
+    fun providesUserAPI(retrofitBuilder: Retrofit.Builder): UserAPI {
+        return retrofitBuilder.build().create(UserAPI::class.java)
+    }
+}
