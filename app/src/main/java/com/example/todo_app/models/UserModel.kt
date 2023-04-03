01ba4c9842dd4ef5a7a11a2268eeb2772a350453
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-04-03 15:26:27 +0800

    Add UserModel

---
+++ app/src/main/java/com/example/todo_app/models/UserModel.kt
@@ -0,0 +1,10 @@
+package com.example.todo_app.models
+
+data class UserModel(
+    val _id: String,
+    val firstName: String,
+    val lastName: String,
+    val email: String,
+    val password: String
+)
+
